58b6aa4522b7f09920434ac56a5fd22f00fa3868
dtinth on MBP M1 <dtinth@spacet.me>
2023-07-01 21:28:31 +0700

    allow loading sitegraph data via postMessage

--- src/$sitegraph.tsx
+++ src/$sitegraph.tsx
@@ -3,16 +3,33 @@ import { atom } from "nanostores";
 import { searchParams } from "./searchParams";
 
 export const $sitegraph = atom<Sitegraph | undefined>();
-fetch(
+const source =
   searchParams.get("sitegraph") ||
-    "https://htrqhjrmmqrqaccchyne.supabase.co/storage/v1/object/public/notes-public/index.graph.json"
-)
-  .then((r) => {
-    if (!r.ok) {
-      throw new Error(`Failed to fetch sitegraph: ${r.status} ${r.statusText}`);
+  "https://htrqhjrmmqrqaccchyne.supabase.co/storage/v1/object/public/notes-public/index.graph.json";
+
+if (source.includes("://")) {
+  fetch(source)
+    .then((r) => {
+      if (!r.ok) {
+        throw new Error(
+          `Failed to fetch sitegraph: ${r.status} ${r.statusText}`
+        );
+      }
+      return r.json();
+    })
+    .then((data) => {
+      $sitegraph.set(Sitegraph.parse(data));
+    });
+} else if (source === "parent.postMessage") {
+  const id = `sitegraphRequest-${Date.now()}-${Math.random()}`;
+  window.addEventListener("message", (e) => {
+    if (e.data?.sitegraph && e.data.id === id) {
+      $sitegraph.set(Sitegraph.parse(e.data.sitegraph));
     }
-    return r.json();
-  })
-  .then((data) => {
-    $sitegraph.set(Sitegraph.parse(data));
   });
+  window.parent.postMessage({ sitegraphRequest: { id } }, "*");
+} else {
+  console.error(
+    'Sitegraph source must be a URL or "parent.postMessage" to receive the sitegraph data via postMessage.'
+  );
+}
