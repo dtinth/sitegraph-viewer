9f1b946403d78ef5042d104f929b59403f73529a
dtinth on MBP M1 <dtinth@spacet.me>
2023-07-02 01:31:19 +0700

    smaller chunk

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,20 @@
-import { defineConfig } from 'vite'
-import preact from '@preact/preset-vite'
+import { defineConfig } from "vite";
+import preact from "@preact/preset-vite";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [preact()],
-})
+  build: {
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          d3: ["d3-force-3d"],
+          preact: ["preact"],
+          nanostores: ["nanostores", "@nanostores/preact"],
+          pixi: ["pixi.js"],
+          zod: ["zod"],
+        },
+      },
+    },
+  },
+});
